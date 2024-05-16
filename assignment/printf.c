701a96279036d39ecddedf84ba6fa174df5ed7e1
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-05-16 17:42:26 +0530

    Delete assignment directory

--- assignment/printf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main() {
-  printf("Name: Karnsinh Rajput ");
-  printf("\n Birth Date: 22/01/2005");
-  printf("\n Age: 19");
-  printf("\n Address: 9/14 gopal nagar,suryamukhi chowk,'momai krupa'");
-}
