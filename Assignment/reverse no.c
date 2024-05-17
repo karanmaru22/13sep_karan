1c702b98b04d8f5adaca400c805852f6f2093a62
karanmaru22 <144922226+karanmaru22@users.noreply.github.com>
2024-05-17 16:53:42 +0530

    Add files via upload

---
+++ Assignment/reverse no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        printf("%d", num % 10);
+        num /= 10;
+    }
+
+    printf("\n");
+
+    return 0;
+}
